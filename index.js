ac6c0566da94cc93ef91a76cc831d1bfc6ac02c9
himelmia535 <himelmia535@gmail.com>
2024-05-13 16:23:49 +0600

    get book by id

--- index.js
+++ index.js
@@ -28,12 +28,12 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
-      //   app.get('/book/:id', async(req, res) => {
-      //     const id = req.params.id;
-      //     const query = {_id: new ObjectId(id)}
-      //     const result = await bookscollection.findOne(query);
-      //     res.send(result);
-      // })
+        app.get('/book/:id', async(req, res) => {
+          const id = req.params.id;
+          const query = {_id: new ObjectId(id)}
+          const result = await bookscollection.findOne(query);
+          res.send(result);
+      })
       // app.post('/book', async (req, res) => {
       //     const newbook = req.body;
       //     console.log(newbook);
